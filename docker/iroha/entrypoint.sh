84ddd8ec64f423a8a3515ffcbf4b02cd3ef5f443
SpaceTimerLoL <73221597+SpaceTimerLoL@users.noreply.github.com>
2022-03-16 02:04:20 +0800

    Update entrypoint.sh

--- docker/iroha/entrypoint.sh
+++ docker/iroha/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sleep 10
-irohad --genesis_block genesis-chainfox.block.json --config config.docker --keypair_name "$KEY" --overwrite-ledger true
+irohad --genesis_block genesis-chainfox.block.json --config config.docker --keypair_name $KEY --overwrite-ledger true
